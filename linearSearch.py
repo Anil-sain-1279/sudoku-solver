212fdccf69ff3a7482192e960ae35138210537ca
Anil_sain_1729 <asain4773@gmail.com>
2025-12-09 22:20:33 +0530

    linearSearch

---
+++ linearSearch.py
@@ -0,0 +1,10 @@
+arr = [10,20,30,40,50]
+x = int(input())
+found = False
+for i in range(len(arr)):
+    if arr[i] == x:
+        print(f"Element found at index {i}")
+        found = True
+        break
+if not found:
+    print("Element not found in the array") 
